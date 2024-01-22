7cfce3d4a6c7af56fca8cc93fb07fd3e164c2b86
Sefux <sfuchs@projektkater.de>
2024-01-22 17:05:03 +0100

    Remove postDeploy hook

--- src/main/java/com/gpe_learn/video_request/Application.java
+++ src/main/java/com/gpe_learn/video_request/Application.java
@@ -6,41 +6,26 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
 
 
-// import com.gpe_learn.video_request.model.Video;
-// import com.gpe_learn.video_request.repository.VideoRepository;
-
 @SpringBootApplication
 @EnableProcessApplication
 public class Application {
 
-  @Autowired
-  private RuntimeService runtimeService;
+  // @Autowired
+  // private RuntimeService runtimeService;
 
 
   public static void main(String... args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @EventListener
-  public void processPostDeploy(PostDeployEvent event) {
-    runtimeService.startProcessInstanceByKey("video_request-process");
-  }
-
-  // 	@Bean
-	// CommandLineRunner commandLineRunner(VideoRepository videoRepository) {
-	// 	return args -> {
-	// 		List<Video> videos = List.of(new Video( "Video1", "https://yt.com/1"), new Video("Titel 2", "https://yt.com/2"));
-	// 		videoRepository.saveAll(videos);
-
-	// 		videoRepository.findAll().stream().forEach(System.out::println);
-	// 	};
-	// }
-
+  // @EventListener
+  // public void processPostDeploy(PostDeployEvent event) {
+  //   runtimeService.startProcessInstanceByKey("video_request-process");
+  // }
 
 }
