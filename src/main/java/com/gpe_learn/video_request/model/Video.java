9ad663e53c8891b146c58f1c003a40ef680025fe
Sefux <sfuchs@projektkater.de>
2024-01-23 11:42:26 +0100

    Add project lombok Refactor model

--- src/main/java/com/gpe_learn/video_request/model/Video.java
+++ src/main/java/com/gpe_learn/video_request/model/Video.java
@@ -6,17 +6,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Video {
 
-    
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String title;
     private String link;
+
     public Video() {
     }
 
@@ -25,33 +29,9 @@ public class Video {
         this.link = link;
     }
 
-    public UUID getId() {
-        return id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getLink() {
-        return link;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
     @Override
     public String toString() {
         return "Video [id=" + id + ", title=" + title + ", link=" + link + "]";
     }
 
-        
-    
 }
